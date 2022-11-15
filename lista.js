8718b87b1bbc36f2952351508f7fa3c68df00a11
LovoC <112988366+LovoC@users.noreply.github.com>
2022-11-15 17:57:34 -0600

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,23 @@
+nombre_alumno_array=[];
+
+
+
+function submit(){
+
+    var ver_alumno_array=[];
+    for(  var N_uno=1; N_uno<=4; N_uno++  ){
+    var T_uno=document.getElementById("nombre_alumno_"+N_uno).value;
+    console.log(nombre_alumno);
+    nombre_alumno_array.push(nombre_alumno);
+
+    }
+
+    console.log(nombre_alumno_array);
+    var longitud_nombre_alumnos_array = nombre_alumno_array.length; 
+    console.log(longitud_nombre_alumnos_array);
+    for(  var N_dos=0; N_dos<=longitud_nombre_alumnos_array; N_dos++ ){
+    
+
+    }
+
+}
